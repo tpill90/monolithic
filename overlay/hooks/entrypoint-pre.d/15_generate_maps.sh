745498e78b902bbcda08f47147f6f0d7b18386fa
Tim Pilius <tpilius@gmail.com>
2024-08-23 09:58:15 -0400

    Refactoring and formatting logging to be easier to read

--- overlay/hooks/entrypoint-pre.d/15_generate_maps.sh
+++ overlay/hooks/entrypoint-pre.d/15_generate_maps.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-IFS=' '
-mkdir -p /data/cachedomains
-echo "Bootstrapping Monolithic from ${CACHE_DOMAINS_REPO}"
+# Maps the various domains per CDN to a single identifier, such as [epic] [blizzard] [steam].  This allows for a better cache hit rate
+# as there will only be one total copy of the data versus one copy per domain.
 
-export GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
+echo "  Bootstrapping Monolithic from ${CACHE_DOMAINS_REPO}"
+
 cd /data/cachedomains
-if [[ ! -d .git ]]; then
-	git clone ${CACHE_DOMAINS_REPO} .
-fi
 
 if [[ "${NOFETCH:-false}" != "true" ]]; then
-	# Disable error checking whilst we attempt to get latest
+    echo "  Pulling latest from cache-domains repo"
+    # Disable error checking whilst we attempt to get latest
 	set +e
 	git remote set-url origin ${CACHE_DOMAINS_REPO}
 	git fetch origin || echo "Failed to update from remote, using local copy of cache_domains"
@@ -25,32 +23,50 @@ OUTPUTFILE=${TEMP_PATH}/outfile.conf
 echo "map \"\$http_user_agent£££\$http_host\" \$cacheidentifier {" >> $OUTPUTFILE
 echo "    default \$http_host;" >> $OUTPUTFILE
 echo "    ~Valve\\/Steam\\ HTTP\\ Client\\ 1\.0£££.* steam;" >> $OUTPUTFILE
-#Next line probably no longer needed as we are now regexing to victory
-#echo "    hostnames;" >> $OUTPUTFILE
-jq -r '.cache_domains | to_entries[] | .key' cache_domains.json | while read CACHE_ENTRY; do 
-	#for each cache entry, find the cache indentifier
+
+jq -r '.cache_domains | to_entries[] | .key' cache_domains.json | while read CACHE_ENTRY; do
+
+    echo ""
+	#for each cache entry, find the cache identifier
 	CACHE_IDENTIFIER=$(jq -r ".cache_domains[$CACHE_ENTRY].name" cache_domains.json)
 	jq -r ".cache_domains[$CACHE_ENTRY].domain_files | to_entries[] | .key" cache_domains.json | while read CACHEHOSTS_FILEID; do
+
 		#Get the key for each domain files
 		jq -r ".cache_domains[$CACHE_ENTRY].domain_files[$CACHEHOSTS_FILEID]" cache_domains.json | while read CACHEHOSTS_FILENAME; do
+
 			#Get the actual file name
-			echo Reading cache ${CACHE_IDENTIFIER} from ${CACHEHOSTS_FILENAME}
+			echo "  Reading domains from ${CACHEHOSTS_FILENAME}"
+
+            # for each file in the hosts file
 			cat ${CACHEHOSTS_FILENAME} | while read CACHE_HOST; do
-				#for each file in the hosts file
-				#remove all whitespace (mangles comments but ensures valid config files)
-				echo "host: $CACHE_HOST"
+                # remove all whitespace (mangles comments but ensures valid config files)
 				CACHE_HOST=${CACHE_HOST// /}
-				echo "new host: $CACHE_HOST"
-				if [ ! "x${CACHE_HOST}" == "x" ]; then
-					#Use sed to replace . with \. and * with .*
-					REGEX_CACHE_HOST=$(sed -e "s#\.#\\\.#g" -e "s#\*#\.\*#g" <<< ${CACHE_HOST})
-					echo "    ~.*£££.*?${REGEX_CACHE_HOST} ${CACHE_IDENTIFIER};" >> $OUTPUTFILE
-				fi
+
+                # Skipping empty lines
+                if [ "x${CACHE_HOST}" == "x" ]; then
+                    continue
+                fi
+
+                # Skipping comments
+                if [[ $CACHE_HOST == \#* ]]; then
+                    continue
+                fi
+
+                echo "    new host: $CACHE_HOST"
+
+                #Use sed to replace . with \. and * with .*
+                REGEX_CACHE_HOST=$(sed -e "s#\.#\\\.#g" -e "s#\*#\.\*#g" <<< ${CACHE_HOST})
+                echo "    ~.*£££.*?${REGEX_CACHE_HOST} ${CACHE_IDENTIFIER};" >> $OUTPUTFILE
+
 			done
 		done
 	done
 done
 echo "}" >> $OUTPUTFILE
+
+echo ""
+echo "Resulting 30_maps.conf file:"
 cat $OUTPUTFILE
+
 cp $OUTPUTFILE /etc/nginx/conf.d/30_maps.conf
 rm -rf $TEMP_PATH
--- overlay/hooks/entrypoint-pre.d/20_perms_check.sh
+++ overlay/hooks/entrypoint-pre.d/20_perms_check.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
+
 if [ -d "/data/cache/cache" ]; then
-	echo "Running fast permissions check"
+	echo "  Running fast permissions check"
 	ls -l /data/cache/cache | tail --lines=+2 | grep -v ${WEBUSER} > /dev/null
 
 	if [[ $? -eq 0 || "$FORCE_PERMS_CHECK" == "true" ]]; then
-		echo "Doing full checking of permissions (This WILL take a long time on large caches)..."
+		echo "  Doing full checking of permissions (This WILL take a long time on large caches)..."
 		find /data \! -user ${WEBUSER} -exec chown ${WEBUSER}:${WEBUSER} '{}' +
-		echo "Permissions ok"
+		echo "  Permissions ok"
 	else
-		echo "Fast permissions check successful, if you have any permissions error try running with -e FORCE_PERMS_CHECK = true"
+		echo "  Fast permissions check successful, if you have any permissions error try running with -e FORCE_PERMS_CHECK = true"
 	fi
 
 fi
